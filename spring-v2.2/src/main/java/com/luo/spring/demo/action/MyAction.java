ef7af64bba0623f5bb9dd00948f702ff3accb9af
archer <352907839@qq.com>
2019-09-03 12:17:24 +0800

    Spring MVC finished 2

--- spring-v2.2/src/main/java/com/luo/spring/demo/action/MyAction.java
+++ spring-v2.2/src/main/java/com/luo/spring/demo/action/MyAction.java
@@ -6,10 +6,14 @@ import com.luo.spring.framework.annotation.ArchAutowired;
 import com.luo.spring.framework.annotation.ArchController;
 import com.luo.spring.framework.annotation.ArchRequestMapping;
 import com.luo.spring.framework.annotation.ArchRequestParam;
+import com.luo.spring.framework.webmvc.servlet.ArchModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 公布接口url
@@ -20,48 +24,58 @@ import java.io.IOException;
 @ArchRequestMapping("/web")
 public class MyAction {
 
-	@ArchAutowired
-	IQueryService queryService;
-	@ArchAutowired
-	IModifyService modifyService;
+	@ArchAutowired IQueryService queryService;
+	@ArchAutowired IModifyService modifyService;
 
 	@ArchRequestMapping("/query.json")
-	public void query(HttpServletRequest request, HttpServletResponse response,
-								@ArchRequestParam("name") String name){
+	public ArchModelAndView query(HttpServletRequest request, HttpServletResponse response,
+								  @ArchRequestParam("name") String name){
 		String result = queryService.query(name);
-		out(response,result);
+		return out(response,result);
 	}
-	
+
 	@ArchRequestMapping("/add*.json")
-	public void add(HttpServletRequest request,HttpServletResponse response,
-			   @ArchRequestParam("name") String name,@ArchRequestParam("addr") String addr){
-		String result = modifyService.add(name,addr);
-		out(response,result);
+	public ArchModelAndView add(HttpServletRequest request,HttpServletResponse response,
+							  @ArchRequestParam("name") String name,@ArchRequestParam("addr") String addr){
+		String result = null;
+		try {
+			result = modifyService.add(name,addr);
+			return out(response,result);
+		} catch (Exception e) {
+//			e.printStackTrace();
+			Map<String,Object> model = new HashMap<String,Object>();
+			model.put("detail",e.getMessage());
+//			System.out.println(Arrays.toString(e.getStackTrace()).replaceAll("\\[|\\]",""));
+			model.put("stackTrace", Arrays.toString(e.getStackTrace()).replaceAll("\\[|\\]",""));
+			return new ArchModelAndView("500",model);
+		}
+
 	}
-	
+
 	@ArchRequestMapping("/remove.json")
-	public void remove(HttpServletRequest request,HttpServletResponse response,
-		   @ArchRequestParam("id") Integer id){
+	public ArchModelAndView remove(HttpServletRequest request,HttpServletResponse response,
+								 @ArchRequestParam("id") Integer id){
 		String result = modifyService.remove(id);
-		out(response,result);
+		return out(response,result);
 	}
-	
+
 	@ArchRequestMapping("/edit.json")
-	public void edit(HttpServletRequest request,HttpServletResponse response,
-			@ArchRequestParam("id") Integer id,
-			@ArchRequestParam("name") String name){
+	public ArchModelAndView edit(HttpServletRequest request,HttpServletResponse response,
+							   @ArchRequestParam("id") Integer id,
+							   @ArchRequestParam("name") String name){
 		String result = modifyService.edit(id,name);
-		out(response,result);
+		return out(response,result);
 	}
-	
-	
-	
-	private void out(HttpServletResponse resp,String str){
+
+
+
+	private ArchModelAndView out(HttpServletResponse resp,String str){
 		try {
 			resp.getWriter().write(str);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return null;
 	}
 	
 }
