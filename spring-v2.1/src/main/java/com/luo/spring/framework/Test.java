c617b2424be14ced5342231bfa1bf6c5638bfbc8
archer <352907839@qq.com>
2019-09-11 12:41:39 +0800

    Spring transaction finished

--- spring-v2.1/src/main/java/com/luo/spring/framework/Test.java
+++ spring-v2.1/src/main/java/com/luo/spring/framework/Test.java
@@ -1,5 +1,6 @@
 package com.luo.spring.framework;
 
+import com.luo.spring.demo.service.impl.QueryService;
 import com.luo.spring.framework.context.ArchApplicationContext;
 
 /**
@@ -13,10 +14,10 @@ public class Test {
     public static void main(String[] args) {
         ArchApplicationContext context = new ArchApplicationContext("classpath:application.properties");
         try {
-//            Object queryService = context.getBean("queryService");
-//            Object queryServiceType = context.getBean(QueryService.class);
-//            System.out.println(queryService);
-//            System.out.println(queryServiceType);
+            Object queryService = context.getBean("queryService");
+            Object queryServiceType = context.getBean(QueryService.class);
+            System.out.println(queryService);
+            System.out.println(queryServiceType);
             Object myAction = context.getBean("myAction");
             System.out.println(myAction);
         } catch (Exception e) {
