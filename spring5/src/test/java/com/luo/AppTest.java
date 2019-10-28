242356fbf56439bb4db359745fdacb2110343739
luoxz <luoxz@akulaku.com>
2019-10-28 09:59:36 +0800

    test change

--- spring5/src/test/java/com/luo/AppTest.java
+++ spring5/src/test/java/com/luo/AppTest.java
@@ -7,8 +7,9 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
+    private static Integer a = 1;
     /**
      * Rigorous Test :-)
      */
@@ -16,5 +17,11 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void getStaticField(){
+        new AppTest().a++;
+        System.err.println(AppTest.a);
     }
 }
